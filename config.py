92ef99f00ec2a5ee25fb4ffad3bbbde0522f9483
逸樵 <oncwnuJ-Vd0rJLFY0kLDDTrrTQZ8@git.weixin.qq.com>
2023-02-06 15:11:49 +0800

    up

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import os
 DEBUG = True
 
 # 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'Fjj111111!')
-db_address = os.environ.get("MYSQL_ADDRESS", '10.3.108.152:3306')
+# username = os.environ.get("MYSQL_USERNAME", 'root')
+# password = os.environ.get("MYSQL_PASSWORD", 'Fjj111111!')
+# db_address = os.environ.get("MYSQL_ADDRESS", '10.3.108.152:3306')
+username = 'root'
+password = 'Fjj111111!'
+db_address = '10.3.108.152:3306'
